07f0d15bac866958fc3c30ba2a74192eff027bb1
baenav <victor_baena@hotmail.com>
2019-09-25 11:01:14 -0500

    Nuevo Rasp-Envio

---
+++ Python/Rasp_Envio.py
@@ -0,0 +1,42 @@
+import socket
+from n611_adquisicion import *
+import time
+from datetime import datetime
+from datetime import timedelta
+
+
+UDP_IP = "3.89.222.210"
+UDP_PORT = 5000
+#UDP_IP2 = "190.143.58.202"
+#UDP_PORT2 = 5011
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+#print("UDP target IP 2:", UDP_IP2)
+#print("UDP target port 2:", UDP_PORT2)
+
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+#try:
+#    sock = socket.socket(socket.AF_INET, # Internet
+#                     socket.SOCK_DGRAM) # UDP
+#except socket.error:
+#    print("Could not bind 2nd IP")
+while True:
+#    MESSAGE = str(adquisicion())
+    try:
+        now = datetime.now()
+        fecha = now.strftime('%Y-%m-%d')
+        hora = time.strftime("%H:%M:%S")
+        ADQ=adquisicion()
+        ADQ=str(ADQ)
+        ADQ=ADQ[1:len(ADQ)-1]
+        MESSAGE = ADQ+', ' + fecha +', '+ hora
+        MESSAGE = bytes(MESSAGE)
+        print("message:", MESSAGE)
+        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        break
+    except socket.error:
+        print "Error de Conexión, reintentando..."
+        time.sleep(1)
+    time.sleep(5)
